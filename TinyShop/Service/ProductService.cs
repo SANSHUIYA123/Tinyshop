3ae1ab05bfb0d537081eb9ee669bcf997e769381
toowoot <1539794351@qq.com>
2020-06-14 13:41:49 +0800

    commit message

---
+++ TinyShop/Service/ProductService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Service
+{
+    public ProductDO Update(ProductDO product)
+    {
+        var productExists:bool= _context.Set<ProductDO>().Any(t: ProductDO => t.Id = product.Id);
+        if (!productExists)
+        {
+            throw new Exception("没有找到指定 ID的产品记录");
+        }
+        _context.Set<ProductDO>().Updata(product);
+        _Context.SaveChanges();
+        return product;
+    }
+    public void Delete(long id) {
+
+        var product = _context.Set<ProductD0>().First0rDefault(t: ProductDO = t.Id = id);
+
+        if (product = null)
+
+            throw new Exception("没有找到指定 ID的产品记录");
+        context.Set<ProductD0>().Remove(product);
+
+        context.SaveChanges();
+    }
+
+    class ProductService
+    {
+        private readonly DataContext_context;
+            public ProductService(DataContext context)
+        {
+            _context = context;
+        }
+        public ProductDO Inset(ProductDO product)
+        {
+            _context.Set<ProductDO>().Add(product);
+            _context.SaveChanges();
+
+            return product;
+        }
+        public IEnumerable<ProductDO> GetAll()
+        {
+            return _context.Set<ProductDO>();
+        }
+    }
+}
---
+++ TinyShop/TinyShop/Controllers/ProductController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TinyShop.Controllers
+{
+    public IActionResult Index()
+    {
+        return ViewComponent();
+    }
+    public IActionResult GetAll()
+    {
+        try
+        {
+            var result:IEnumerable < ProductDO >= _productService.GetAll();
+            return JsonOptions(new
+            {
+                code = "success",
+                data = result
+            });
+        }
+        catch (Exception ex) { }
+        return Json(new
+        {
+            code = "fail",
+            message = ex.Message
+        });
+    }
+    public class ProductController : Controller
+    {
+        public IActionResult GetProductById(long id) {
+            try {
+                var product = _productService.GetById(id);
+
+                return Json(new
+                {
+
+                    code = "success",
+                    data = product
+                });
+            }
+
+            catch (Exception ex) {
+                return Json(new {
+
+                    code = "fail",
+
+
+                 message = ex.Message });
+            }
+        }
+
+        public IActionResult Edit(long id) {
+            ViewBag.Id = id;
+
+            return View();
+        }
+            public IActionResult GetProductById(long id) ftry
+
+            var product = _ productService.GetById(id);
+
+            return Json(new f
+            
+
+            code = "success"data = product
+
+catch (Exception ex) í
+
+return Json(new (
+
+code = "fail"
+
+message = ex.Message
+
+public IActionResult Edit(long id) iViewBag.Id = id;
+
+            return View();
+
+            [HttpPost]
+
+            public IActionResult Delete(long id)
+            {
+                try
+                {
+
+                    _productService.Delete(id);
+
+                    return Json(new
+                    {
+
+                        code = "success",
+                    });
+                }
+
+                catch (Exception ex)
+                {
+                    return Json(new
+                    {
+
+                        code = "fail",
+
+                        message = ex.Message
+                    });
+                }
+            }
+
+        private ProductService _productService;
+
+        public ProductController(DataContext context)
+        {
+            _productService = new ProductService(context);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ProductVO product)
+        {
+            var productDO = new ProductDO
+            {
+                ProductNumber = product.ProductNumber,
+                ProductName = product.ProductName,
+                Price = Convert.ToDouble(product.Price),
+                Price = product.Price
+            };
+
+            try
+            {
+                var insertedProduct = _productService.Insert(productDO);
+
+                return Json(new
+                {
+                    code = "success",
+                    data = insertedProduct
+                });
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    code = "fail",
+                    message = ex.Message
+                });
+            }
+        }
+    }
+}
+
